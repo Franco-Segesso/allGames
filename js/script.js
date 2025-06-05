2eaab0b3b6d578e19e9c8bd21138fabbdb1ba77c
Franco Segesso <fransege@gmail.com>
2025-06-04 21:02:16 -0300

    incorporacion de js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+/*
+let numeroA= parseInt(prompt("Ingrese un numero: "));
+let numeroB=parseInt(prompt("Ingrese un numero: "));
+let resultado=numeroA+numeroB;
+
+if (resultado>10){
+    alert(resultado);
+}else if (resultado==10){
+    alert("El resultado es igual a 10");
+    
+}else{
+    alert("El resultado es menor a 10");
+}
+*/
+
+
+/*
+document.writeln(resultado);
+*/
+
+
+/*
+let resultado=confirm("Desea salir?");
+console.log(resultado);*/
